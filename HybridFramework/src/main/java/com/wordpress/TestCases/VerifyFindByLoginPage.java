4cdc79d229f2d2fe78a1d05e160d7bb5ccb836cc
Kumarrahul94 <rahulkramesh94@gmail.com>
2020-08-15 14:43:31 +0530

    few comment removed

--- HybridFramework/src/main/java/com/wordpress/TestCases/VerifyFindByLoginPage.java
+++ HybridFramework/src/main/java/com/wordpress/TestCases/VerifyFindByLoginPage.java
@@ -14,15 +14,11 @@ public class VerifyFindByLoginPage extends BasePage{
 	@Test
 	public void checkValidateUser() {
 		
-		System.out.println("Performing git action");
 		logger = report.createTest("Login to CRM");
 		LoginPageFindBy login = PageFactory.initElements(driver, LoginPageFindBy.class);
-		
 		logger.info("Starting Application");
 		login.login_wordpress("admin","demo123");
-		
 		logger.pass("Logged In Successully");
-		
 		System.out.println("Test Case Ended");
 	}	
 	
